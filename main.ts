05f033c7e757086af1f0b9bfdf1ccb28f56a5a60
Sandal Ghani <162090167+SandalGhani@users.noreply.github.com>
2024-05-13 19:52:29 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,27 @@
+#!  /usr/bin/env node
+
+import inquiere from "inquirer";
+
+const answer = await inquiere.prompt([
+  { message: "Enter firstNumber", type: "number", name: "firstNumber" },
+  { message: "Enter secondNumber", type: "number", name: "secondNumber" },
+  {
+    message: "Select one of the  oprators to perform operation",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+
+//conditional statement
+if (answer.operator === "Addition") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "Subtraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+} else {
+  console.log("Please select valid oprator");
+}
